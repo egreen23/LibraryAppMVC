31c3517629270dc6e338a2603d3591c180de84e4
Niko Gentile <nikogentile@libero.it>
2024-11-28 17:45:40 +0100

    cartella con migration scripts

---
+++ LibraryAppMVC/scripts/20241108145433_AddAuthorTable.sql
@@ -0,0 +1,43 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Author] (
+    [Id] int NOT NULL IDENTITY,
+    [Nome] nvarchar(30) NOT NULL,
+    [Cognome] nvarchar(50) NOT NULL,
+    [Nazionalita] nvarchar(max) NOT NULL,
+    [DoB] date NOT NULL,
+    [LuogoNascita] nvarchar(max) NOT NULL,
+    [DoD] date NULL,
+    CONSTRAINT [PK_Author] PRIMARY KEY ([Id])
+);
+GO
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Cognome', N'DoB', N'DoD', N'LuogoNascita', N'Nazionalita', N'Nome') AND [object_id] = OBJECT_ID(N'[Author]'))
+    SET IDENTITY_INSERT [Author] ON;
+INSERT INTO [Author] ([Id], [Cognome], [DoB], [DoD], [LuogoNascita], [Nazionalita], [Nome])
+VALUES (1, N'Maximo', '2006-10-11', NULL, N'Birmingham', N'inglese', N'Steve'),
+(2, N'Terry', '1942-10-30', '1984-12-21', N'Londra', N'inglese', N'Harry'),
+(3, N'Margiotta', '1954-05-07', '1994-12-31', N'Birmingham', N'italiana', N'Mario'),
+(4, N'Henry', '1931-12-01', '1962-02-03', N'Birmingham', N'francese', N'Thierry');
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Cognome', N'DoB', N'DoD', N'LuogoNascita', N'Nazionalita', N'Nome') AND [object_id] = OBJECT_ID(N'[Author]'))
+    SET IDENTITY_INSERT [Author] OFF;
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20241108145433_AddAuthorTable', N'8.0.10');
+GO
+
+COMMIT;
+GO
+
---
+++ LibraryAppMVC/scripts/20241108150908_AddBookTable.sql
@@ -0,0 +1,37 @@
+﻿BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Book] (
+    [Id] int NOT NULL IDENTITY,
+    [Titolo] nvarchar(70) NOT NULL,
+    [Quantita] int NOT NULL,
+    [DataPubblicazione] date NOT NULL,
+    [Genere] nvarchar(max) NOT NULL,
+    [Prezzo] decimal(18,2) NOT NULL,
+    [AuthorId] int NULL,
+    CONSTRAINT [PK_Book] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Book_Author_AuthorId] FOREIGN KEY ([AuthorId]) REFERENCES [Author] ([Id])
+);
+GO
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'AuthorId', N'DataPubblicazione', N'Genere', N'Prezzo', N'Quantita', N'Titolo') AND [object_id] = OBJECT_ID(N'[Book]'))
+    SET IDENTITY_INSERT [Book] ON;
+INSERT INTO [Book] ([Id], [AuthorId], [DataPubblicazione], [Genere], [Prezzo], [Quantita], [Titolo])
+VALUES (1, 1, '1999-04-02', N'Fantasia', 1.3, 3, N'Biancaneve'),
+(2, 1, '1988-02-15', N'Fantasia', 1.8, 3, N'Cenerentola'),
+(3, 2, '2004-04-22', N'Thriller', 3.0, 1, N'Oxford Murderers'),
+(4, 3, '1921-06-13', N'Romanzo', 2.5, 3, N'Orgoglio e Pregiudizio');
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'AuthorId', N'DataPubblicazione', N'Genere', N'Prezzo', N'Quantita', N'Titolo') AND [object_id] = OBJECT_ID(N'[Book]'))
+    SET IDENTITY_INSERT [Book] OFF;
+GO
+
+CREATE INDEX [IX_Book_AuthorId] ON [Book] ([AuthorId]);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20241108150908_AddBookTable', N'8.0.10');
+GO
+
+COMMIT;
+GO
+
